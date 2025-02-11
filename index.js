8cffe59fb05390d41e9549edd53904628e2dd7de
JINX23606 <jinxgenius23@outlook.co.th>
2025-02-11 22:56:49 +0700

    save project

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const PORT = 4000
-app.listen(PORT,()=>{
-    console.log(`API Listening on PORT${PORT}`)
+
+app.listen(PORT, () => {
+    console.log(`API Listening on PORT ${PORT}`)
 })
-app.get('/',(req,res)=>{
-    res.send('สวัสดีฮ้าบ')
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
 })
-app.get('/about',(req,res)=>{
+
+app.get('/about', (req, res) => {
     res.send('This is my about route')
 })
 
